d45a571e7b98d12fd45af0c07a6956c77f694e2b
Peter Westmacott <peter.westmacott@plumbee.co.uk>
2016-04-08 15:26:32 +0100

    Add project file.

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject com.plumbee/plumbot "0.0.1"
+  :description "A Clojure library for hosting Slack bots."
+  :url "http://plumbee.com"
+  :license {:name "Apache License"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/core.async "0.2.374"]
+                 [com.stuartsierra/component "0.3.1"]
+                 [org.slf4j/slf4j-api "1.7.21"]
+                 [org.eclipse.jetty.websocket/websocket-client "9.3.0.M2"]
+                 [ring/ring-codec "1.0.0"]])
