a232c6c2f09ea2fbc83b35e2df685ba988d95c27
Peter Westmacott <peter.westmacott@plumbee.co.uk>
2016-04-14 10:24:54 +0100

    Add signing key for deploy to Clojars.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://plumbee.com"
   :license {:name "Apache License"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :signing {:gpg-key "tech-external@plumbee.co.uk"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/core.async "0.2.374"]
